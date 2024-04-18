d9c04a228bc9443d3ce50dc07b6cb4273147b0fe
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-04-17 20:54:48 -0600

    Creating styled Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,29 @@
-export default function Input() {
-  return <div>Hola</div>;
+import {
+  Box,
+  TextField,
+  TextFieldProps,
+  TextFieldVariants,
+  Typography,
+} from "@mui/material";
+
+import style from "./styles.module.scss";
+
+export default function Input(props: TComponentProps) {
+  return (
+    <Box>
+      <Typography
+        fontFamily="inherit"
+        fontWeight="bold"
+        color="#f20574"
+        marginBottom={1}
+      >
+        {props.title}
+      </Typography>
+      <TextField fullWidth className={style["text-field"]} {...props} />
+    </Box>
+  );
 }
+
+type TComponentProps = {
+  variant?: TextFieldVariants;
+} & Omit<TextFieldProps, "variant" | "className"> & { title: string };
