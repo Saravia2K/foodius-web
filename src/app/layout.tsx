8b7567179e365db6270eb24f39f06b926f55207d
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-04-29 04:53:38 -0600

    feat: setting up providers and global layout settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import ClientNavbar from "@/layouts/ClientNavbar";
+import Footer from "@/layouts/Footer";
+
 import "./globals.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import ToastifyProvider from "@/providers/ToastifyProvider";
 
-const inter = Poppins({ subsets: ["latin"], weight: "200" });
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "400", "600", "800"],
+});
 
 export const metadata: Metadata = {
   title: "Foodius",
@@ -13,7 +20,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: TRootLayout) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToastifyProvider>
+          <ClientNavbar />
+          {children}
+          <Footer />
+        </ToastifyProvider>
+      </body>
     </html>
   );
 }
