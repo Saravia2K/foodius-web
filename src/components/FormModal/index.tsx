2d4ab4fdbb21e83db704ff1e6c59f41cf2defbde
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-04-29 04:55:35 -0600

    feat: adding error alert

--- src/components/FormModal/index.tsx
+++ src/components/FormModal/index.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import { Box, Modal, IconButton } from "@mui/material";
+import { Box, Modal, IconButton, Alert } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
 import sxStyles from "./styles";
@@ -9,6 +9,7 @@ import Image from "next/image";
 
 export default function FormModal({
   open,
+  error,
   children,
   onClose,
 }: TComponentProps) {
@@ -16,6 +17,7 @@ export default function FormModal({
     <Modal open={open} onClose={onClose}>
       <Box p={6} sx={sxStyles.box}>
         <Image src={logo} alt="Foodius logo" className={styles.logo} />
+        {error && <Alert severity="error">{error}</Alert>}
         {children}
         <IconButton onClick={onClose} sx={sxStyles.iconBtn}>
           <CloseIcon />
@@ -27,5 +29,6 @@ export default function FormModal({
 
 type TComponentProps = PropsWithChildren<{
   open: boolean;
+  error?: string;
   onClose: () => void;
 }>;
