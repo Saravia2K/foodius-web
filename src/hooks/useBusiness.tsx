41a7510364232c56e69d14707b8b70b6bfce9643
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-05-08 07:30:36 -0600

    feat: using api data

--- src/hooks/useBusiness.tsx
+++ src/hooks/useBusiness.tsx
@@ -1,7 +1,48 @@
-import NEGOCIOS from "@/data/NEGOCIOS.json";
+import { APIRequest } from "@/utils";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
-export default function useBusiness() {
+export default function useBusiness(slug: string) {
+  const url = `businesses/${slug}`;
+  const { data, isLoading } = useQuery({
+    queryKey: ["businesses", slug],
+    queryFn: () => APIRequest<TBusinessResponse>(url, "GET"),
+    staleTime: Infinity,
+    placeholderData: keepPreviousData,
+  });
+
   return {
-    business: NEGOCIOS,
+    business: data?.data!,
+    businessLoading: isLoading,
   };
 }
+
+export type TBusinessResponse = {
+  id: number;
+  name: string;
+  email: string;
+  phone_number: string;
+  location: string;
+  password: string;
+  logo: string;
+  banner: string;
+  slug: string;
+  Schedules: [];
+  FoodCategory: TFoodCategory[];
+};
+
+export type TFoodCategory = {
+  name: string;
+  description: string;
+  Food: TFood[];
+};
+
+export type TFood = {
+  id: number;
+  id_food_category: number;
+  name: string;
+  description: string;
+  price: string;
+  extra_fee: string;
+  img_url: string;
+  is_available: boolean;
+};
