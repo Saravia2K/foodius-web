d278963e316245c08f206e2b9ac8e14cb85c2675
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-05-20 08:51:07 -0600

    chore: layout directly rendered on client

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,6 +1,22 @@
+"use client";
+
 import { PropsWithChildren } from "react";
-import Main from "./components/Main";
+import { Box } from "@mui/material";
+import useGlobalSettings from "@/hooks/useGlobalSettings";
+import Footer from "@/layouts/Footer";
 
 export default function ClientLayout({ children }: PropsWithChildren) {
-  return <Main>{children}</Main>;
+  const { mainStyles } = useGlobalSettings((s) => s);
+
+  return (
+    <Box
+      component="main"
+      bgcolor="var(--white)"
+      className="main"
+      sx={mainStyles}
+    >
+      {children}
+      <Footer />
+    </Box>
+  );
 }
