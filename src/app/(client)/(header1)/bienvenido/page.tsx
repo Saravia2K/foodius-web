52d785d0fdacaa22d5220d2ed1a6e41511506aca
chris <christophervallecios@outlook.com>
2024-06-04 22:53:59 -0600

    page bienvenidos y unete

--- src/app/(client)/(header1)/bienvenido/page.tsx
+++ src/app/(client)/(header1)/bienvenido/page.tsx
@@ -1,5 +1,144 @@
-import styles from "./styles.module.scss";
+"use client";
+import Accordion from "react-bootstrap/Accordion";
+import ColoredButton from "@/components/ColoredButton";
+
+import Image from "next/image";
+import mascotafoodius from "@/assets/images/mscfoodius.png";
+import tipohumo from "@/assets/images/tipoconhumo.png";
+import dostipos from "@/assets/images/dostipos.png";
 
 export default function BienvenidoPage() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container-fluid bg-white py-5">
+        <div className="row">
+          <div className="col-md-6 mt-5">
+            <h1
+              className="card-title py-4"
+              style={{ fontSize: "80px", padding: "20px" }}
+            >
+              <strong>Bienvenido</strong>{" "}
+              <strong style={{ color: "#F20574" }}>#FoodiusPartner.</strong>
+            </h1>
+          </div>
+          <div className="col-md-6">
+            <center>
+              <Image
+                src={mascotafoodius}
+                alt="grafica"
+                style={{ width: "500px", height: "300px" }}
+              />
+            </center>
+          </div>
+        </div>
+        <center>
+          <div style={{ marginTop: "60px", marginBottom: "60px" }}>
+            <ColoredButton color="pink">Administra tu negocio</ColoredButton>
+          </div>
+        </center>
+      </div>
+
+      <div className="container-fluid mt-4">
+        <div className="row">
+          <div className="col-md-6 mt-5 py-5">
+            <center>
+              <Image
+                src={tipohumo}
+                alt="grafica"
+                style={{
+                  width: "500px",
+                  height: "300px",
+                  borderRadius: "10px",
+                }}
+              />
+            </center>
+          </div>
+          <div className="col-md-6 mt-5 py-5">
+            <center>
+              <p style={{ margin: "40px", fontSize: "20px" }}>
+                Crece con Foodies Amplía tu alcance al incluir tu negocio en la
+                aplicación de Foodies. Aumenta las ventas conectándote con los
+                clientes que buscan opciones de pickup. Cada asociación incluye
+                nuestro sistema de pedidos en línea personalizable.
+              </p>
+            </center>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="container-fluid mt-5"
+        style={{ backgroundColor: "#F20574" }}
+      >
+        <div className="row">
+          <div className="col-md-6 mt-5 py-5 ">
+            <center>
+              <h1 style={{ color: "white" }}>
+                <strong>#FoodiusPartner</strong>
+              </h1>
+              <p style={{ margin: "40px", fontSize: "20px", color: "white" }}>
+                "Sin duda, Foodies ha superado nuestras expectativas al
+                ayudarnos en cada situación que se presenta, y más allá de eso,
+                nos han brindado gran apoyo en cuanto a marketing, branding y
+                difusión de Favoritos."
+                <br></br>
+                <br></br>
+                Foodius Partners.{" "}
+              </p>
+            </center>
+          </div>
+          <div className="col-md-6 mt-2 py-5">
+            <center>
+              <Image
+                src={dostipos}
+                alt="grafica"
+                style={{
+                  width: "500px",
+                  height: "300px",
+                  borderRadius: "10px",
+                }}
+              />
+            </center>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid  bg-white py-5">
+        <center>
+          <h1 style={{ color: "black" }}>
+            <strong>Preguntas frecuentes</strong>
+          </h1>
+        </center>
+
+        <center>
+          <div className="w-50 pt-4">
+            <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                  ¿Cobramos algún tipo de tarifa?
+                </Accordion.Header>
+                <Accordion.Body>
+                  En efecto, en nuestra plataforma aplicamos una comisión por el
+                  uso de nuestros servicios.{" "}
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>
+                  ¿Dónde puedo ir si tengo más preguntas?
+                </Accordion.Header>
+                <Accordion.Body>
+                  Si deseas ponerte en contacto con nosotros, puedes utilizar
+                  nuestros medios de comunicación:
+                  <br></br>
+                  Correo electrónico: foodius@email.com
+                  <br></br>
+                  Teléfono: +503 xxxx-xxxx{" "}
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+        </center>
+      </div>
+    </div>
+  );
 }
--- src/app/(client)/(header1)/unete/page.tsx
+++ src/app/(client)/(header1)/unete/page.tsx
@@ -1,5 +1,130 @@
+import Input from "@/components/Input";
 import styles from "./styles.module.scss";
+import ColoredButton from "@/components/ColoredButton";
+import Image from "next/image";
+import graficas from "../../../../assets/images/grffoodius.png";
 
 export default function UnetePage() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="p-3">
+              <center>
+                <h1 style={{ fontSize: "100px", marginTop: "120px" }}>
+                  <strong>Se parte de</strong>
+                </h1>
+              </center>
+              <center>
+                <h1 style={{ color: "#F20574", fontSize: "120px" }}>
+                  <strong>#Foodius</strong>
+                </h1>
+              </center>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="p-3">
+              <div className="card rounded-5 border-0  p-3">
+                <div className="card-body">
+                  <h6 className="card-title border-bottom py-4">
+                    Asociate a foodius para impulsar el crecimiento y llevar tus{" "}
+                    <strong style={{ color: "#F20574" }}>
+                      Antojitos a un nuevo nivel.
+                    </strong>
+                  </h6>
+                </div>
+
+                <Input
+                  title="Nombre del negocio"
+                  placeholder="ingrese el nombre"
+                  type="text"
+                  style={{ margin: "10px", padding: "10px" }}
+                />
+
+                <Input
+                  title="Ubicacion del negocio"
+                  placeholder="Departamento / ciudad"
+                  type="text"
+                  style={{ margin: "10px", padding: "10px" }}
+                ></Input>
+                <Input
+                  title="Correo electronico"
+                  placeholder="example@gmail.com"
+                  type="email"
+                  style={{ margin: "10px", padding: "10px" }}
+                ></Input>
+                <Input
+                  title="Telefono comercial"
+                  placeholder="xxxx-xxxx"
+                  type="tel"
+                  style={{ margin: "10px", padding: "10px" }}
+                ></Input>
+                <Input
+                  title="Logo"
+                  placeholder="ingrese la imagen"
+                  type="image"
+                  style={{ margin: "10px", padding: "10px" }}
+                ></Input>
+                <Input
+                  title="Banner"
+                  placeholder="ingrese la imagen"
+                  type="image"
+                  style={{ margin: "10px", padding: "10px" }}
+                ></Input>
+
+                <ColoredButton
+                  color="pink"
+                  style={{ margin: "20px", padding: "10px" }}
+                >
+                  Registrar negocio
+                </ColoredButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid bg-white py-5">
+        <center>
+          <h1 style={{ fontSize: "80px", marginTop: "60px" }}>
+            <strong>Unete a </strong>{" "}
+            <strong style={{ color: "#F20574" }}>#Foodius </strong>{" "}
+            <strong> y despierta el hambre de nuevos clientes</strong>
+          </h1>
+        </center>
+
+        <div className="row">
+          <div className="col-md-6 mt-5 py-5">
+            <center>
+              <h1
+                style={{ fontSize: "35px", marginTop: "40px", margin: "40px" }}
+              >
+                <strong>Brilla en el </strong>{" "}
+                <strong style={{ color: "#F20574" }}>Menu Digital </strong>{" "}
+                <strong> : Aumenta tu Visibilidad con foodius</strong>
+              </h1>
+            </center>
+            <center>
+              <p style={{ margin: "40px" }}>
+                Al unirte a foodius, tu restaurante o negocio ganara visibilidad
+                y alcanzara a una amplia audiencia de clientes potenciales que
+                buscan opciones gastronomicas locales.
+              </p>
+            </center>
+          </div>
+
+          <div className="col-md-6 py-5">
+            <center>
+              <Image
+                src={graficas}
+                alt="grafica"
+                style={{ width: "400px", height: "400px" }}
+              />
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
