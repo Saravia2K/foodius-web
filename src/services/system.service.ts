a662752aa28faa14bb558282a490b2895ae21520
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-06-11 16:27:00 -0600

    feat: auth middleware

--- src/services/system.service.ts
+++ src/services/system.service.ts
@@ -17,3 +17,23 @@ export const checkLogin = async (email: string) => {
   const { message } = (await fetchRes.json()) as { message: "string" };
   return message;
 };
+
+export const setCookie = async (key: string, value: string) => {
+  await fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ key, value }),
+  });
+};
+
+export const removeCookie = async (key: string) => {
+  await fetch("/api/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ key }),
+  });
+};
